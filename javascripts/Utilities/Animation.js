1bc964322e145822cead34a7b30f69a9b6c2afe3
Lonewolf5432 <bilsonc47@gmail.com>
2019-12-13 12:34:54 +0545

    single loop animation made

--- javascripts/Utilities/Animation.js
+++ javascripts/Utilities/Animation.js
@@ -1,6 +1,6 @@
 class Animation
 {
-    constructor(spriteSheet,sourceHeight,sourceWidth,height,width,noOfFrame)
+    constructor(spriteSheet,sourceHeight,sourceWidth,height,width,noOfFrame,repeat=true)
     {
         this.image=new Image();
         this.image.src=spriteSheet;
@@ -11,6 +11,7 @@ class Animation
         this.frameIndex=0;
         this.maxNoOfFrame=noOfFrame;
         this.tickCount=0;
+        this.repeat=repeat;
     }
     draw(canvasContext,positionX,positionY)
     {
@@ -21,8 +22,12 @@ class Animation
         {
             this.frameIndex++;
             if(this.frameIndex>=this.maxNoOfFrame)
+            {
                 this.frameIndex=0;
-
+                if(!this.repeat)
+                    this.frameIndex=this.maxNoOfFrame-1;
+            }
+               
             this.tickCount=0;
         }
     }
